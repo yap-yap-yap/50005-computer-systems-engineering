6971d4eb1337aef33e1a885dd47e07077b123310
yap-yap-yap <zhihanyap@gmail.com>
2021-03-14 18:17:20 +0800

    countline: fixed formatting on output

--- ProgrammingAssignment1/PA1/shellPrograms/shellCountLine_code.c
+++ ProgrammingAssignment1/PA1/shellPrograms/shellCountLine_code.c
@@ -20,7 +20,7 @@ int shellCountLine_code(char **args)
     char *line = NULL;
     size_t len = 0;
     int count = 0;
-    printf("%s", args[1]);
+    printf("%s\n", args[1]);
     // 1. Given char** args, open file in READ mode based on the filename given in args[1] using fopen()
     FILE *fp = fopen(args[1], "r");
     // 2. Check if file exists by ensuring that the FILE* fp returned by fopen() is not NULL
@@ -29,14 +29,20 @@ int shellCountLine_code(char **args)
         // 4. Loop, as long as getline() does not return -1, keeps reading and increment the count
         while((nread = getline(&line, &len, fp)) != -1){
             count++;
-            fwrite(line, nread, 1, stdout);
+            // fwrite(line, nread, 1, stdout);
         }
     }
     // 6. Close the FILE*
     fclose(fp);
 
     // 7. Print out how many lines are there in this particular filename
-    printf("There are %d lines %s\n", count, args[1]);
+    // selects appropriate grammar depending on the count value
+    if(count == 1){
+        printf("There is %d line in %s\n", count, args[1]);
+    }
+    else{
+        printf("There are %d lines in %s\n", count, args[1]);
+    }
     return 1;
 }
 
